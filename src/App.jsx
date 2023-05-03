dbc5f61e0db9a84ccc31d8997f0c6dcd8877894c
Brayan Lopez Piza <brayan.lopez.pizza@gmail.com>
2023-05-02 20:17:09 -0500

    second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
-import Pokedex from './pages/pokedex'
+import Pokedex from './pages/Pokedex'
 import ProtectedAuth from './components/auth/ProtectedAuth'
 import PokemonId from './pages/PokemonId'
 
